555eb6aeb76a146afb768f3a909344392b4a4d12
Kevin Liao <liaok0082@gmail.com>
2021-02-03 15:27:09 -0300

    API fix error

--- network/routes.js
+++ network/routes.js
@@ -4,10 +4,10 @@ const posts = require('../api/components/posts/network');
 const comments = require('../api/components/comments/network');
 
 const routes = server => {
-    server.use('/api/users', user);
-    server.use('/api/auth', auth);
-    server.use('/api/posts', posts);
-    server.use('/api/comments', comments);
+    server.use('api/users', user);
+    server.use('api/auth', auth);
+    server.use('api/posts', posts);
+    server.use('api/comments', comments);
 }
 
 module.exports = routes; 
